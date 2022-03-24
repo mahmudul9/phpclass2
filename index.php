cde3ef029b790c0b6f689a141b961992619962f1
github123 <123github>
2022-03-25 01:38:22 +0600

    25/3/22

--- index.php
+++ index.php
@@ -1,4 +1,17 @@
-<?php
-echo "hello world";
+ <!-- I have three old hp monitors, not new. I want to sell it on my own website. That’s why I should create a product upload form on my website, then upload my product with specification and available quantity. My monitors have a 15.6 inch display. My monitor’s VGA cable status, yes, I will sell it for 8000 tk.
 
- ?>
+For creating a product upload form what specifications will be uploaded and what will be their data types?  -->
+
+<?php
+$monitorQuantity = "three";  //Datatype is string
+
+$moniter_name = "hp" ;       //Datatype is string
+
+$Is_moniter_condition_old = true;   //Datatype is Boolean
+
+$Moniter_size =   15.6;      //Datatype is double/float
+
+$moniter_cable_status= "VGA";//Datatype is string or
+$is_moniter_cable_status_VGA = true; //Datatype is Boolean
+
+$sellPrice = 8000 ;         //Datatype is integer
